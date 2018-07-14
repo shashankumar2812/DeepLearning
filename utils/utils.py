6d9ead6918361fb5f2717831972350824feee9f8
Shashank Kumar <35758713+shashankumar2812@users.noreply.github.com>
2018-07-14 12:51:14 -0400

    Initial commit
    Sigmoid, sigmoid derivative, Softmax, L1 and L2 loss functions

---
+++ utils/utils.py
@@ -0,0 +1,111 @@
+#  sigmoid
+
+import numpy as np 
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    
+
+    s = 1/(1+(np.exp(-x)))
+    
+    
+    return s
+
+
+   #  sigmoid_derivative
+
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+    
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+    
+
+    s = sigmoid(x)
+    ds = s*(1-s)
+    
+    
+    return ds
+
+
+  #  softmax
+
+def softmax(x):
+    """Calculates the softmax for each row of the input x.
+
+    Your code should work for a row vector and also for matrices of shape (n, m).
+
+    Argument:
+    x -- A numpy matrix of shape (n,m)
+
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (n,m)
+    """
+    
+
+    # Apply exp() element-wise to x. Use np.exp(...).
+    x_exp = np.exp(x)
+
+    # Create a vector x_sum that sums each row of x_exp. Use np.sum(..., axis = 1, keepdims = True).
+    x_sum = np.sum(x_exp, axis=1, keepdims=True)
+    
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    s = x_exp/x_sum
+
+    
+    
+    return s
+
+
+ #  L1 Loss
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    
+
+    loss = sum(abs(yhat- y))
+    
+    
+    return loss
+
+
+ #  L2 loss
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    
+
+    loss = sum((abs(yhat-y))**2)
+    
+    
+    return loss
+
+
